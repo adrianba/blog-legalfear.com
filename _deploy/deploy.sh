955fcbeb8381af25d56b97406ebdf098c7ab6101
Ade Bateman <bateman@acm.org>
2020-07-31 12:20:01 -0700

    Initial commit.

---
+++ _deploy/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+# Update publish branch
+STATUS=`git log -1 --pretty=oneline`
+cd _site
+git config user.name "Travis-CI"
+git config user.email "travis-ci"
+git add .
+git commit -m "Built by Travis-CI: $STATUS"
+git status
+GH_REPO="@github.com/adrianba/blog-legalfear.com.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+git push $FULL_REPO publish:publish
