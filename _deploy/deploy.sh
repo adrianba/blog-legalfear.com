8f09c5c78477e1401b19ab212ab3af32acecb6a1
Ade Bateman <bateman@acm.org>
2020-08-03 10:45:26 -0700

    Add publish step.

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+if [[ -z "${GH_TOKEN}" ]]; then
+  echo "Missing GH_TOKEN"
+  exit 1
+fi
+
 # Update publish branch
 STATUS=`git log -1 --pretty=oneline`
 cd _site
-git config user.name "Travis-CI"
-git config user.email "travis-ci"
+git config user.name "GitHub Action"
+git config user.email "GitHub Action"
 git add .
-git commit -m "Built by Travis-CI: $STATUS"
+git commit -m "Built by GitHub Action: $STATUS"
 git status
 GH_REPO="@github.com/adrianba/blog-legalfear.com.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
-git push $FULL_REPO publish:publish
+git push --verbose $FULL_REPO publish:publish
